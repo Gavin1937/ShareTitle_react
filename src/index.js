dca2a71109ceb120d729fd24edd300d46cfcff16
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-02-03 22:26:49 -0800

    add routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 import Main from './pages/Main'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={ <Navigate to="/sharetitle" /> } />
+      <Route exact path="/sharetitle" element={ <Main /> } />
+    </Routes>
+  </Router>
 );
-
