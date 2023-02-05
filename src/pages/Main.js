a9a504736577310c62727cecd8c490a15b4b3e4b
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-02-04 21:42:48 -0800

    add is_visited filter

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,12 +7,13 @@ import ShareTitleTable from '../components/ShareTitleTable';
 function Main() {
   const [ready, setReady] = useState(false);
   const [finished, setFinished] = useState(false);
-  const [query, setQuery] = useState({page:0});
+  const [query, setQuery] = useState({page:0, order:"DESC", is_visited:"unvisited"});
   const [payload, setPayload] = useState(0);
   
   useEffect(() => {
     async function initialFetch() {
-      let response = await axios.get(`/api/query`);
+      let queryStr = new URLSearchParams(query).toString();
+      let response = await axios.get(`/api/query?${queryStr}`);
       setPayload(response.data);
       setFinished(true);
       setReady(true);
@@ -20,12 +21,36 @@ function Main() {
     initialFetch();
   }, []);
   
-  async function handleSubmit(e) {
-    e.preventDefault();
-    
+  // update table every time updating state query
+  useEffect(() => {
+    //! debug message
+    console.log("updating table...", query);
+    updateTable();
+  }, [query]);
+  
+  
+  async function updateTable() {
     setReady(false);
     setFinished(false);
     setPayload({})
+    
+    // request from backend
+    let queryStr = new URLSearchParams(query).toString();
+    try {
+      let response = await axios.get(`/api/query?${queryStr}`);
+      setPayload(response.data);
+      setFinished(true);
+    }
+    catch (err) {
+      console.log(err);
+    }
+    
+    setReady(true);
+  }
+  
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
     
     // parse search value
     let searchValue = document.querySelector(".SearchBar > textarea").value;
@@ -48,55 +73,49 @@ function Main() {
     if (("page" in _query) == false) {
       _query["page"] = 0;
     }
-    setQuery(_query);
     
-    // request from backend
-    let queryStr = new URLSearchParams(query).toString();
-    try {
-      let response = await axios.get(`/api/query?${queryStr}`);
-      setPayload(response.data);
-      setFinished(true);
-      console.log(response.data);
+    await setQuery(_query);
+  }
+  
+  async function handleFilterSelection(event) {
+    let value = event.target.value;
+    if (value === "visited" || value === "unvisited") {
+      await setQuery(prev => {
+        let obj = JSON.parse(JSON.stringify(prev));
+        delete obj.is_visited;
+        delete obj.page;
+        return {"is_visited":value, "page":0, ...obj};
+      });
     }
-    catch (err) {
-      console.log(err);
+    else if (value === "all") {
+      // remove is_visited key from object
+      await setQuery(prev => {
+        let obj = JSON.parse(JSON.stringify(prev));
+        delete obj.is_visited;
+        delete obj.page;
+        return {"page":0, ...obj};
+      });
     }
-    
-    setReady(true);
   }
   
   async function handlePageMove(event) {
-    
-    setReady(false);
-    setFinished(false);
-    setPayload({})
-    
     // change page by offset
     let pageOffset = parseInt(event.target.value);
     let _query = query;
     if (_query.page + pageOffset <= 0) {
-      _query.page = 0
+      await setQuery(prev => {
+        let obj = JSON.parse(JSON.stringify(prev));
+        obj.page = 0;
+        return obj;
+      });
     }
     else {
-      _query.page += pageOffset;
+      await setQuery(prev => {
+        let obj = JSON.parse(JSON.stringify(prev));
+        obj.page += pageOffset;
+        return obj;
+      });
     }
-    await setQuery(_query);
-    console.log(pageOffset);
-    console.log(query);
-    
-    // request from backend
-    let queryStr = new URLSearchParams(query).toString();
-    try {
-      let response = await axios.get(`/api/query?${queryStr}`);
-      setPayload(response.data);
-      setFinished(true);
-      console.log(response.data);
-    }
-    catch (err) {
-      console.log(err);
-    }
-    
-    setReady(true);
   }
   
   return (
@@ -107,6 +126,14 @@ function Main() {
           <SearchBar className="SearchBar" width={"80%"} height={"5%"} />
           <button type="submit">Submit</button>
         </form>
+      </div>
+      <div className="fileter">
+        <p>filter</p>
+        <select defaultValue="unvisited" onChange={handleFilterSelection}>
+          <option key="1" value="unvisited">unvisited</option>
+          <option key="2" value="visited">visited</option>
+          <option key="3" value="all">all</option>
+        </select>
       </div>
       <div className="PageMover-1">
         <button className="prev" type="button" value="-1" onClick={handlePageMove}>
