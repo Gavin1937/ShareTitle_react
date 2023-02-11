1bfa1686fe966ecf27c3473ac1e3da6803533853
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-02-11 02:24:40 -0800

    change routing to start with '/sharetitle'

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route exact path="/" element={ <Navigate to="/sharetitle" /> } />
-      <Route exact path="/sharetitle" element={ <Main /> } />
-      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/sharetitle" element={ <Navigate to="/sharetitle/home" /> } />
+      <Route exact path="/sharetitle/home" element={ <Main /> } />
+      <Route exact path="/sharetitle/login" element={ <Login /> } />
     </Routes>
   </Router>
 );
